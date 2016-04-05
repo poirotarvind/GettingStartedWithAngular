ceea4403c171587398af2bb1bd4aa749a8057487
poirotarvind <poirotarvind@hotmail.com>
2016-04-05 06:50:50 +0530

    Added files via upload

---
+++ AngularJS/Scripts/JavaScript.js
@@ -0,0 +1,7 @@
+﻿(function SimpleController($scope) {
+    $scope.numbers = [
+                { nam: 'One', v: '1' },
+                { nam: 'Two', v: '2' },
+                { nam: 'Three', v: '3' },
+                { nam: 'Four', v: '4' }];
+});
